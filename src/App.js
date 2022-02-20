31ec9559d4a5cdb872b251ae86ee016c379f919f
Chris Catechis <catecc1@unlv.nevada.edu>
2022-02-20 13:20:48 -0800

    Full commit.
    This is all I have for the app, will probably not be making this better. May add a branch to create a like button if I have time today.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Tweet from "./tweet";
 
-function App() {
+function App(){
+
+  // const [likesGoyle, setLikesGoyles]= useState(0);
+  
+  // const incrementGoyle = () => {
+  //   setLikesGoyles(likesGoyle + 1);  // in use of like button
+  // }
+
+  const [users, setUsers] = useState([
+    { name: "goyle", message:"here's a hidden doc I found", likes:"likes: 4" },
+    { name: "Ian", message:"checkout this tiktok tinyurl.malware", likes:"likes: 12" },
+    { name: "Snowden", message:"in russia", likes:"likes: 134" },
+    { name: "rubik", message:"what do the numbers mean?", likes:"likes: 0" },
+    { name: "Vonnegut", message:"If this isn't nice, I don't know what is.", likes:"likes: 789" }
+  ]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "app">
+      <h1>hidden truth</h1>
+      {/* <button onClick={incrementGoyle}>like</button> */}
+      {users.map(user => (  // traverse array of users
+        <Tweet name={user.name} message={user.message} likes={user.likes} />
+      ))}
     </div>
   );
 }
